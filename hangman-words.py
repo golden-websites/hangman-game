51b08fe211491b53c7e691b44e540614f4a68f96
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-02-14 10:08:45 +0430

    The word lists of the game
    Lists of words from which the random words will be generated for the user to guess.

---
+++ hangman-words.py
@@ -0,0 +1,54 @@
+word_list = [
+	# a
+	'apple','ant','anchor','angle','animal','april','avocado','azure','amber','axiom',
+	# b
+	'banana','balloon','band','bank','basil','beacon','beetle','bench','binary','biscuit',
+	# c
+	'cactus','candle','canvas','canyon','carrot','castle','catalyst','cave','cedar','census',
+	# d
+	'dagger','daisy','damage','dancer','danger','dawn','decade','decline','delta','design',
+	# e
+	'eagle','early','earth','echo','ecology','editor','elegant','element','ellipse','ember',
+	# f
+	'fabric','falcon','fable','family','fancy','farmer','faucet','feather','feline','ferment',
+	# g
+	'galaxy','garden','garlic','garnet','gather','gauze','gavel','gentle','geology','gesture',
+	# h
+	'habit','harbor','harvest','harmony','hazard','helmet','heritage','hollow','honest','horizon',
+	# i
+	'iceberg','icon','idea','ideal','ignite','illusion','image','impact','impose','indigo',
+	# j
+	'jacket','jaguar','jargon','jasmine','jealous','jelly','jersey','jetty','jewel','jigsaw',
+	# k
+	'kangaroo','karate','karma','kayak','kernel','keyboard','kettle','kingdom','kiosk','kismet',
+	# l
+	'label','labor','ladder','lagoon','lambda','lament','lantern','laptop','larva','legacy',
+	# m
+	'machine','magnet','magic','major','mammal','manage','mansion','maple','marine','matrix',
+	# n
+	'nacho','naive','narrow','nation','native','nebula','nectar','needle','neon','nexus',
+	# o
+	'oasis','object','oblige','ocean','octave','oddity','offer','office','olive','omega',
+	# p
+	'paddle','pageant','palace','palm','panel','panther','parcel','parent','parrot','particle',
+	# q
+	'quack','quartz','query','quest','queue','quick','quiet','quilt','quirky','quota',
+	# r
+	'rabbit','radar','radiant','random','range','rapid','raven','react','realm','rebel',
+	# s
+	'sable','safari','saddle','safari','salsa','salmon','sample','sanity','sapphire','satisfy',
+	# t
+	'table','tackle','tactic','talent','tango','target','tartan','tavern','teacher','temple',
+	# u
+	'ubuntu','udder','ugly','ultra','umbrella','unison','unit','unite','unique','universe',
+	# v
+	'vacuum','vague','valley','valor','vampire','vanish','vanilla','vault','vector','velvet',
+	# w
+	'wagon','waiter','walnut','wander','warrior','water','waver','wealth','weather','whale',
+	# x
+	'xanthan','xavier','xenon','xenial','xenodochial','xerox','xigua','xylophone','xylem','xeniality',
+	# y
+	'yacht','yam','yard','yarn','yarrow','yearn','yeast','yellow','yodel','youth',
+	# z
+	'zebra','zenith','zero','zest','zigzag','zinc','zipper','zone','zodiac','zombie'
+]
